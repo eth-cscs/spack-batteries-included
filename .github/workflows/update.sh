3d2741aef5a4c505185dd4de8d26109209cbb235
Harmen Stoppels <harmenstoppels@gmail.com>
2021-09-03 17:40:39 +0200

    Update workflow

--- .github/workflows/update.sh
+++ .github/workflows/update.sh
@@ -9,16 +9,16 @@ SPACK_SHA=$(git ls-remote https://github.com/spack/spack.git | grep refs/heads/d
 echo "Using spack SHA $SPACK_SHA"
 
 # Download the latest stable spack.x release and the latest runtime.x
-export BASE_VERSION="v2.3.1"
+export BASE_VERSION="v2.3.2"
 echo "Downloading spack-$ARCH.x from version $BASE_VERSION"
 
 curl -LfSs -o "runtime-$ARCH-fuse2"  "https://github.com/haampie/spack-batteries-included/releases/download/$BASE_VERSION/runtime-$ARCH-fuse2"
 curl -LfSs -o "runtime-$ARCH-fuse3"  "https://github.com/haampie/spack-batteries-included/releases/download/$BASE_VERSION/runtime-$ARCH-fuse3"
 curl -LfSs -o "spack-$ARCH-old.x"    "https://github.com/haampie/spack-batteries-included/releases/download/$BASE_VERSION/spack-$ARCH.x"
 
-echo "aa269f3af2303925ec8752ed326200a91f65896a17ac80583bfe15d79c161470 runtime-$ARCH-fuse2"  | sha256sum --check
-echo "aa89a9add9d3a44351df31d29953081fdf9bf1d41a479721f655faaf52851932 runtime-$ARCH-fuse3"  | sha256sum --check
-echo "c7fff39dd7bbdeb9b795a956dd7ca4a76aae37d106c8a04744b3bc6b7690aae1 spack-$ARCH-old.x"    | sha256sum --check
+echo "3f3be2781919f3ed47a4400f41227f01581e2f34d1beb2cf6988bbdae6514e8d runtime-$ARCH-fuse2"  | sha256sum --check
+echo "334d32b0fba99539e4112f420299a14ad938ba140eb8c4ef0c9fa7f53d180728 runtime-$ARCH-fuse3"  | sha256sum --check
+echo "4e075729dba6c2c2b7383063c0a1342167cb8375c7c61880e860d451aa5d830b spack-$ARCH-old.x"    | sha256sum --check
 
 chmod +x "spack-$ARCH-old.x"
 export PATH="$PWD:$PATH"
